e47b4739acc1f7b50ad3e179f0ed9dda46138c1b
jholt <holtkampjs@gmail.com>
2022-02-15 12:59:37 -0600

    Updated outward facing API

--- CLI_test.go
+++ CLI_test.go
@@ -1,29 +1,31 @@
-package poker
+package poker_test
 
 import (
 	"strings"
 	"testing"
+
+	"github.com/holtkampjs/poker"
 )
 
 func TestCLI(t *testing.T) {
 
 	t.Run("record chris win from user input", func(t *testing.T) {
 		in := strings.NewReader("Chris wins\n")
-		playerStore := &StubPlayerStore{}
+		playerStore := &poker.StubPlayerStore{}
 
-		cli := &CLI{playerStore, in}
+		cli := *poker.NewCLI(playerStore, in)
 		cli.PlayPoker()
 
-		assertPlayerWin(t, playerStore, "Chris")
+		poker.AssertPlayerWin(t, playerStore, "Chris")
 	})
 
 	t.Run("record chris win from user input", func(t *testing.T) {
 		in := strings.NewReader("Cleo wins\n")
-		playerStore := &StubPlayerStore{}
+		playerStore := &poker.StubPlayerStore{}
 
-		cli := &CLI{playerStore, in}
+		cli := *poker.NewCLI(playerStore, in)
 		cli.PlayPoker()
 
-		assertPlayerWin(t, playerStore, "Cleo")
+		poker.AssertPlayerWin(t, playerStore, "Cleo")
 	})
 }
